a13deb60b1b788539aae61ca22633014adc9223c
Nicolas Cirulli <nicolascirulli2@gmail.com>
2026-03-31 02:17:40 -0300

    Resolucion contar-duplicados

--- ejercicios-algoritmos/sesion-3/contar-duplicados/index.ts
+++ ejercicios-algoritmos/sesion-3/contar-duplicados/index.ts
@@ -1,4 +1,21 @@
 export default function contarDuplicados(texto: string): number {
   // TODO: implementar
-  return 0;
+  const aux = texto
+    .split("")
+    .map((letra) =>
+      letra
+        .normalize("NFD")
+        .replace(/[\u0300-\u036f]/g, "")
+        .toLowerCase(),
+    )
+    .reduce((acc, act) => {
+      if (acc[act]) {
+        acc[act] += 1;
+      } else {
+        acc[act] = 1;
+      }
+      return acc;
+    }, {});
+  const result = Object.entries(aux).filter((e: {}) => e[1] >= 2).length;
+  return result;
 }
