386863b14e4f1d24595adf2c54380fde81d97f6f
Nicolas Cirulli <nicolascirulli2@gmail.com>
2026-03-30 21:32:42 -0300

    Resolucion con bonus pero sin test bonus

--- ejercicios-algoritmos/sesion-2/continentes-representados/index.ts
+++ ejercicios-algoritmos/sesion-2/continentes-representados/index.ts
@@ -9,5 +9,43 @@ type Registry = {
 
 export default function continentesRepresentados(array: Registry[]): boolean {
   // TODO: implement
-  return false;
+  const continentes = {
+    Africa: false,
+    Americas: false,
+    Asia: false,
+    Europe: false,
+    Oceania: false,
+  };
+  for (const persona of array) {
+    continentes[persona.continent] = true;
+  }
+  return Object.values(continentes).every((v) => v);
+}
+// Bonus 1
+function desarrolladoresDeContinente(
+  registro: Registry[],
+  lenguaje: string,
+  continente: string
+) {
+  return registro.filter(
+    ({ language, continent }) =>
+      language === lenguaje && continent === continente
+  ).length;
+}
+interface RegistyExtended extends Registry {
+  greeting: string;
+}
+// Bonus 2
+export function registroActualizado(array: Registry[]): RegistyExtended[] {
+  return array.map((el) => {
+    return {
+      ...el,
+      greeting: `Hi ${el.firstName}, what do you like the most about ${el.language}?`,
+    };
+  });
+}
+
+//Bonus 3
+export function lenguajesRepresentados(array: Registry[]) {
+  return Array.from(array.map((e) => e.language));
 }
